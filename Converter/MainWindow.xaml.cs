c2a31af6133bdef5b6b438572ea6a387765b2bb3
Benjamin Stern <benjstern@users.noreply.github.com>
2020-06-07 01:22:12 +0300

    Initial Development with Linking to the MongoDB database

--- Converter/MainWindow.xaml.cs
+++ Converter/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Converter.Service;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,38 @@ namespace Converter
     /// </summary>
     public partial class MainWindow : Window
     {
+        private SefariaMongoDBService _serviceMongo;
         public MainWindow()
         {
             InitializeComponent();
+
+            _serviceMongo = new SefariaMongoDBService();
+            
+        }
+
+        private void Convert_OnClick(object sender, RoutedEventArgs e)
+        {
+            
+            var count = _serviceMongo.TextsCount();
+            Log("text count: " + count);
+            for (int i = 0; i < count; i++)
+            {
+                var text = _serviceMongo.GetTextAt(i);
+                Log("text at: " + i + " count: " + text.Elements.ToList().Count);
+                for (int j = 0; j < text.Elements.ToList().Count; j++)
+                {
+                    Log(" element: name: " + text.GetElement(j).Name + " value: " + text.GetElement(j).Value);
+                }
+                
+                
+            }
+            
+        }
+
+        private void Log(string msg)
+        {
+            Trace.WriteLine(msg);
+            logTxt.Text += msg + "<LineBreak/>";
         }
     }
 }
---
+++ Converter/SefariaMongoDBService.cs
@@ -0,0 +1,71 @@
+﻿using Converter.Model.MongoDB;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Converter.Service
+{
+    //https://docs.microsoft.com/en-us/aspnet/core/tutorials/first-mongo-app?view=aspnetcore-3.1&tabs=visual-studio
+    class SefariaMongoDBService
+    {
+        private readonly IMongoCollection<BsonDocument> _texts;
+        public SefariaMongoDBService()
+        {
+            var client = new MongoClient("mongodb://localhost:27017");
+            var database = client.GetDatabase("sefaria");
+
+            _texts = database.GetCollection<BsonDocument>("texts");
+            var amount = _texts.CountDocuments(new BsonDocument());
+        }
+
+        public long TextsCount()
+        {
+            return _texts.CountDocuments(new BsonDocument()); 
+        }
+
+        public BsonDocument GetTextAt(int index) {
+            return _texts.Find(_ => true).Skip(index).FirstOrDefault();
+        }
+    }
+}
+
+namespace Converter.Model.MongoDB
+{
+    [BsonIgnoreExtraElements]
+    class Text {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id;
+
+        [BsonElement("language")]
+        public string Language;
+
+        [BsonElement("title")]
+        public string Title;
+
+        [BsonElement("versionSource")]
+        public string VersionSource;
+
+        [BsonElement("versionTitle")]
+        public string VersionTitle;
+
+        [BsonElement("versionTitleInHebrew")]
+        public string VersionTitleHebrew;
+
+        [BsonElement("chapter")]
+        [BsonRepresentation(BsonType.Document)]
+        public List<Chapter> Chapter;
+    }
+    [BsonIgnoreExtraElements]
+    class Chapter {
+
+        public string Value;
+        public List<Chapter> List;
+    }
+
+
+}
